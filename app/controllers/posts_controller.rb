efc7b56b7c0cf9313eb4a5b6abab3ac4185028c5
Andreas <aw@blackboxdev.co>
2016-01-28 19:23:58 +0800

    Initial Styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -41,7 +41,7 @@ class PostsController < ApplicationController
   private
     
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :subtitle)
     end 
 
     def find_post
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128082518) do
+ActiveRecord::Schema.define(version: 20160128091836) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "subtitle"
   end
 
 end
